47bf3b4dc208c6bea6c9e3d84b28d9e306999418
anhhk123 <87219078+anhhk123@users.noreply.github.com>
2021-07-10 12:44:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: '😘😘😘😘',
+    messDesc: '😘😘😘😘😘',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/caovantuananh18' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
